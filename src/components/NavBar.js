e622282e2ddccc2c2a5d5b7a3e1e13081192d5d4
Mushfiq Khan <mushfiqk@Mushfiqs-MacBook-Air.local>
2024-10-12 21:32:38 -0400

    Basic home page files set up with React routing

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+      <nav className="navbar">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/photos">Photography</a></li>
+          <li><a href="/blog">Blog</a></li>
+        </ul>
+      </nav>
+    );
+  }
+  
+export default NavBar;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to mkh4n.com</h1>
+      <p>This is the currently horribly broken home page.</p>
+      <p>Don't bother trying to open the links in the navbar.</p>
+      <p>There will eventually be some nice looking content here (hopefully).</p>
+    </div>
+  );
+}
+
+export default Home;
